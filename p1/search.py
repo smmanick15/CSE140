4891199890a2bb7d9ba8f8b904323b71db9b30e5
Smrithi Manickam <smrithimanickam@eduroam-169-233-153-51.ucsc.edu>
2023-04-18 18:30:36 -0700

    BFS: cleaned up code, added comments

--- p1/search.py
+++ p1/search.py
@@ -30,83 +30,59 @@ def breadthFirstSearch(problem):
     Search the shallowest nodes in the search tree first. [p 81]
     """
 
-    print("Start: %s" % (str(problem.startingState())))
-    print("Is the start a goal?: %s" % (problem.isGoal(problem.startingState())))
-    print("Start's successors: %s" % (problem.successorStates(problem.startingState())))
-    print(problem.successorStates(problem.startingState()))
+    # print("Start: %s" % (str(problem.startingState())))
+    # print("Is the start a goal?: %s" % (problem.isGoal(problem.startingState())))
+    # print("Start's successors: %s" % (problem.successorStates(problem.startingState())))
+    # print(problem.successorStates(problem.startingState()))
 
     # *** Your Code Here ***
 
     start_node = (problem.startingState(), '', 0)
-    print("to make sure we initialized the start node right... let's try printing it!!!\n\n\n")
-    print(start_node)
     frontier = Queue()
-    i = 0
-    frontier.push(start_node)
+    frontier.push(start_node)        # initialize queue with start node
     
-    reached = []
-    reached.append(problem.startingState())
-    recentNodes = Stack()
+    reached = []          # initialize list to check whether states have been visited    
+    reached.append(problem.startingState()) # initialize list with starting state
+    recentNodes = Stack()   # initialize stack used to track nodes from goal state to origin state
     recentNodes.push(start_node)
     foundGoal = False
 
-    while not frontier.isEmpty():
+    while not frontier.isEmpty():       # while fringe is not empty, do the following
         node = frontier.pop()
-        print("the node we just popped is: ", node)
-        #i = 0
         state = node[0]
-        print(state)
-        print(problem.successorStates(state))
-        print(frontier.__len__())
-        for child in problem.successorStates(state):
+        for child in problem.successorStates(state):    # check each child node of parent node
             goal = child
             childState = child[0]
-            print("inside for loop, printing child: ", childState)
-            if problem.isGoal(childState):
-                print("did we ever find the goal...? YES WE DIDDDDDD LET'S CELEBRATE")
-                print("let's make sure we have the right goal though...: ", child)
-                print("what are the successor states of this child?, see below\n")
-                print(problem.successorStates(childState))
+            if problem.isGoal(childState):     # if child state is goal, push child to visited nodes stack and break
                 recentNodes.push(child)
                 foundGoal = True
                 break
-            else:
+            else:                              # if child is not goal, append child if it is a new state (not seen before)
                 if (childState not in reached):
-                    print("we did not find", childState, "in reached")
                     reached.append(childState)
                     frontier.push(child)
                     recentNodes.push(child)
         if (foundGoal == True):
             break
 
-
-    print("\n\n\nabout to print s after exiting graph traversal: ", childState)
-    print(reached)
-    print("lets look at the length of the recent nodes visited: ", recentNodes.__len__())
-
-    back_state = childState
-    actions = []
+    back_state = childState        
+    actions = []                    # list of actions to return
     
-    while (back_state != problem.startingState()):
+    while (back_state != problem.startingState()):  # while the current state is not the origin, do:
         compare_node = recentNodes.pop()
-        print("this is the node we are comparing ", back_state, "to: ", compare_node)
-        print(problem.successorStates(back_state))
-        if (back_state == compare_node[0]):
+        if (back_state == compare_node[0]):         # if current state is the last node visited, add action
             actions.append(compare_node[1])
         found_next = False
         for child in problem.successorStates(back_state):
-            if (child[0] == compare_node[0]):       #check if successor state is valid
-                print("YAY! We found a valid state\n")
-                back_state = compare_node[0]
-                if (compare_node[1] != ''):
+            if (child[0] == compare_node[0]):       # check if successor state is valid
+                back_state = compare_node[0]        # if valid, set current state to successor state
+                if (compare_node[1] != ''):         # add action to list of results
                     actions.append(compare_node[1])
                 found_next = True
                 break
     
-    actions.reverse()
-    print("printing returned list of actions: ", actions)
+    actions.reverse()               # reverse order of action steps
     return actions
-
 
     # raise NotImplementedError()
 
